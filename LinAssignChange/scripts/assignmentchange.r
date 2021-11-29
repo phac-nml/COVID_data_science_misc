6c26bc802785f34fcd8c899a541f5f38b0c52791
chichen <chih-yu.chen@canada.ca>
2021-11-29 13:10:02 -0600

    modified:   LinAssignChange/scripts/COVID19_assignmentThroughTime_report.Rmd 	modified:   LinAssignChange/scripts/assignmentchange.r

--- LinAssignChange/scripts/assignmentchange.r
+++ LinAssignChange/scripts/assignmentchange.r
@@ -14,8 +14,9 @@ require(here)
 ########
 ### Set your own input directory, example data is in the data folder
 ########
-#inputDir="W:/Projects/covid-19/analysis/Ongoing_Pangolin_IRIDA_Updates" ### NML only
-inputDir=here("data")
+inputDir="W:/Projects/covid-19/analysis/Ongoing_Pangolin_IRIDA_Updates" ### NML only
+#inputDir=here("data")
+
 
 ########
 ## acquiring folder names of pangolin results
@@ -23,20 +24,13 @@ inputDir=here("data")
 timev<-dir(inputDir,pattern="^pangolin_analysis")
 timevclean<-substr(gsub("pangolin_analysis_2021_","",timev),1,5) ## narrowing down to month_day
 
-########
-## By default: the output directory is the latest pangolin output folder through name sorting
-## You can set your own
-########
-outputDir=file.path(inputDir,tail(timev,1))
+  
+  ## should there be two runs on the same day, chose the later one by default
+  repDates<-names(which(table(timevclean)>1))
+  toskip<-sapply(repDates,function(y){ tmp<-which(y==timevclean);tmp[-length(tmp)]})
+  timev<-timev[-toskip]
+  timevclean<-timevclean[-toskip]
 
-linFocusName=""
-########
-## You can choosing to show all lineages or a subset of lineages that had changes
-########
-# all
-linFocus <- NULL 
-# Subset: If you only want to examine samples that have been assigned to a subset of lineages at time points selected. Use the script below
-#linFocus <- c("AY.74","B.1.617.2","AY.45"); linFocusName=paste0("_",paste(linFocus,collapse="_"))
 
 ########
 ### select the subset of data to compare: choose one of the following, last5 is chosen by default
@@ -47,6 +41,24 @@ linFocus <- NULL
 selectT<-(length(timev)-4):length(timev); typeselect="last5" ## select the last five time points
 
 
+
+########
+## By default: the output directory is the latest pangolin output folder within your selected runs through name sorting
+## You can set your own
+########
+outputDir=file.path(inputDir,tail(timev[selectT],1))
+
+
+
+########
+## You can choosing to show all lineages or a subset of lineages that had changes
+########
+# all
+linFocus <- NULL;linFocusName=""
+# Subset: If you only want to examine samples that have been assigned to a subset of lineages at time points selected. Use the script below
+#linFocus <- c("AY.74","B.1.617.2","AY.45"); linFocusName=paste0("_",paste(linFocus,collapse="_"))
+
+
 ### maximum Sankey pixel in height, let this be
 maxSankeyPx=5000 ## max 5000px, otherwise it's too long.
 
@@ -54,5 +66,5 @@ maxSankeyPx=5000 ## max 5000px, otherwise it's too long.
 ### run the Rmd file, saving both sankey plot and the html report in the output directory
 ### Please note the report is meant for all lineages and not a subset of lineages. When running a lineage subset, the html report file name has the lineages added to it.
 ########
-render("scripts/COVID19_assignmentThroughTime_report.Rmd",output_file=paste0("COVID19_assignmentThroughTime_report",linFocusName,".html"), output_dir = outputDir, params = list(output_dir = outputDir))
+render("scripts/COVID19_assignmentThroughTime_report.Rmd",output_file=paste0("COVID19_assignmentThroughTime_report_",typeselect,"_",linFocusName,".html"), output_dir = outputDir, params = list(output_dir = outputDir))
 
